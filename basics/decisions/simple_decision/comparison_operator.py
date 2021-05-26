03cc58420015f67f6d2cc7c5ef981c4ffd88967e
Amadeus91 <>
2021-05-26 22:01:34 +0000

    Added code for a little more complex program.

---
+++ basics/decisions/simple_decision/comparison_operator.py
@@ -0,0 +1,10 @@
+print("Please enter the first number.")
+a = int(input())
+print("Please enter the second number.")
+b = int(input())
+if (a < b):
+  print("The first number is the smallest.")
+elif (a > b):
+  print("The second number is the smallest.")
+else:
+  print("Both numbers are equal.")
