fe865363ca5d990ed4547108455daf45da915546
Amadeus91 <>
2021-05-28 17:54:06 +0000

    Added code to demonstrate the use of a simple while loop.

---
+++ basics/repetitions/while_loop/count.py
@@ -0,0 +1,7 @@
+print("How many cables should I remove?")
+n = int(input())
+i = 0
+print()
+while (i < n):
+  print("Removed cable.")
+  i = i + 1
---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,7 @@
+print("How many bars shoud be charged?")
+n = int(input())
+i = 0 
+while (i < n):
+  i = i + 1
+  print("Charging:", "^" * i)
+print("\nBattery fully charged.")
