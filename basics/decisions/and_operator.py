727a6c41d429cba0d3d3e6bbd10ff47bae4a297b
Amadeus91 <>
2021-05-28 17:08:16 +0000

    Added code to demonstrate the use of the and operator.

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,8 @@
+print("What did I hear?")
+a = input()
+print("\nWhat did I see?")
+b = input()
+if ((a == "grrr") and (b == "two red eyes")):
+  print("\nThere is a scary creature. I should get out of here!")
+else: 
+  print("\nI am a little scared but I will continue.")
